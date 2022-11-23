bcab97140974febf17e7822842d1962cc2f62dfd
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-11-23 09:32:07 +0300

    commit

--- src/routes/host/[id]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { customAlphabet } from "nanoid";
-	const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const nanoid = customAlphabet(alphabet, 8);
-	console.log(nanoid());
-</script>
