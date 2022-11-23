a6e7741875a554b2825146e57ceaa249c6fc1e7e
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-11-23 15:47:44 +0300

    commit

---
+++ src/routes/join/[id]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { io } from "socket.io-client";
+
+	const socket = io(`localhost:3000/ms-online-${$page.params.id}`);
+
+	socket.connect();
+	socket.on("connect", () => {
+		console.log("connected");
+	});
+
+	socket.on("message", (data) => {
+		console.log("data received: " + data);
+	});
+
+	socket.send("message");
+</script>
---
+++ src/routes/test/[id]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Coordinate from "$lib/Coordinate/Coordinate";
+	import MinesweeperBoard from "$lib/MinesweeperBoard";
+	import { io } from "socket.io-client";
+
+	const socket = io(`localhost:3000/`);
+
+	socket.connect();
+	socket.on("connect", () => {
+		console.log("connected");
+	});
+
+	socket.emit(
+		"sendMinesweeperBoard",
+		new MinesweeperBoard(20, 10, new Coordinate(2, 4, -2))
+	);
+</script>
