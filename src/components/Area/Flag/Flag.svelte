74505ec3cc7cbc391548fbb1bfeff425d387a40c
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-11-25 10:37:36 +0300

    commit

--- src/components/Area/Flag/Flag.svelte
+++ src/components/Area/Flag/Flag.svelte
@@ -17,6 +17,6 @@ padding-bottom: ${vv / (multiplier * 2)}${vt};
     border-top: ${vv / (multiplier * 3)}${vt} solid transparent;
     border-bottom: ${vv / (multiplier * 3)}${vt} solid transparent;
     border-left: ${vv / multiplier}${vt} solid ${flagColor};
-    transform: translate(-1px, 1px);
-    `} />
+    transform: translate(-1px, 1px); 
+    `} /><!-- TODO: write the transform in terms of vv and vt -->
 </div>
