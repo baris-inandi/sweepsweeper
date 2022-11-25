f11128f36294f25fd6737fe13779d52b18010719
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-11-25 20:52:32 +0300

    commit

---
+++ concurrently.cjs
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const concurrently = require("concurrently");
+concurrently(
+	[
+		{
+			command: "npm run dev",
+			name: "S",
+			prefixColor: "blue",
+			cwd: path.resolve(__dirname, "server")
+		},
+		{
+			command: process.argv.includes("--host")
+				? "npm run vite-host"
+				: "npm run vite-dev",
+			name: "V",
+			prefixColor: "magenta",
+			cwd: path.resolve(__dirname)
+		}
+	],
+	{
+		killOthers: ["failure"],
+		restartTries: 2,
+		restartDelay: 500,
+		cwd: path.resolve(__dirname, "scripts")
+	}
+);
--- server/build.cjs
+++ server/build.cjs
@@ -1,13 +1,13 @@
 const { typecheckPlugin } = require("@jgoz/esbuild-plugin-typecheck");
 
-require("esbuild")
-	.build({
-		entryPoints: ["src/server.ts"],
-		bundle: true,
-		platform: "node",
-		outfile: "dist/index.js",
-		plugins: [typecheckPlugin()]
-	})
-	.then((result) => {
-		console.log("esbuild: Build successful");
-	});
+require("esbuild").build({
+	entryPoints: ["src/server.ts"],
+	bundle: true,
+	platform: "node",
+	outfile: "dist/index.js",
+	plugins: [
+		typecheckPlugin({
+			omitStartLog: true
+		})
+	]
+});
