458f58472a0da940c36f7ad7cfa80d08aa799277
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-11-25 19:42:14 +0300

    commit

--- server/build.cjs
+++ server/build.cjs
@@ -1,9 +1,12 @@
+const { typecheckPlugin } = require("@jgoz/esbuild-plugin-typecheck");
+
 require("esbuild")
 	.build({
 		entryPoints: ["src/server.ts"],
 		bundle: true,
 		platform: "node",
-		outfile: "dist/index.js"
+		outfile: "dist/index.js",
+		plugins: [typecheckPlugin()]
 	})
 	.then((result) => {
 		console.log("esbuild: Build successful");
